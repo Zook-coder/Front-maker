fc506134bf9cff7956e17d7af8a7312014570252
Naïman <85957227+NaimanMpl@users.noreply.github.com>
2024-10-15 16:16:48 +0200

    ci: added eslint github action (#2)

---
+++ eslint.config.mjs
@@ -0,0 +1,41 @@
+import pluginJs from "@eslint/js";
+import parser from '@typescript-eslint/parser';
+import eslintPluginPrettier from "eslint-plugin-prettier/recommended";
+import pluginReact from "eslint-plugin-react";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+
+export default [
+  { 
+    files: ["**/*.{ts,tsx}"],
+    languageOptions: {
+      parser,
+      parserOptions: {
+        project: true
+      }
+    }
+  },
+  { languageOptions: { globals: { ...globals.browser, ...globals.node } } },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    ...pluginReact.configs.flat.recommended,
+    settings: {
+      react: {
+        version: "detect",
+      },
+    },
+    rules: {
+      ...pluginReact.configs.flat.recommended.rules,
+      "react/react-in-jsx-scope": "off",
+    },
+  },
+  eslintPluginPrettier,
+  {
+    ignores: [
+      'eslint.config.mjs',
+      '.next/**',
+      '**/*.{js,mjs}',
+    ],
+  }
+];
