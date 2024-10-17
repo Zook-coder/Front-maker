914a0287ac68323620ccd61470719393465e12d8
JeremyBala <148952702+JeremyBala@users.noreply.github.com>
2024-10-17 11:34:13 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -3,9 +3,9 @@ from player import Player
 from evilman import Evilman
 
 # Creating the objects
-protector = Protector(name="Guardian", power="Shield of Light")
+protector = Protector(name="Guardian", power1="Speed++ !", power2="Stop the Evilman for 5 seconds !", power3="Invisibility !")
 player = Player(name="Alex", objective="Find a job")
-evilman = Evilman(name="Dark Lord", power="Shadow of Chaos")
+evilman = Evilman(name="Dark Lord", power1="Stop the player !", power2="Slow Motion !", power3="Make an obstacle appear !")
 
 # Using the methods
 protector.help_player(player)
