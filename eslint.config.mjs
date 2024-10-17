1c48d597ee2c63e86a4bbd3dd24ef04a14d4a18b
Naïman <85957227+NaimanMpl@users.noreply.github.com>
2024-10-17 15:28:16 +0200

    feat: adjust queries handling and lobby states (#8)

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -36,7 +36,7 @@ export default [
       'eslint.config.mjs',
       '.next/**',
       '**/*.{js,mjs}',
-      "./src/components/ui/**/*.{ts, tsx}"
+      "src/components/ui/**/*.{tsx}"
     ],
   }
 ];
