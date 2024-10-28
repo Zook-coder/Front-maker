908ecdd8e6dd3d6b4eb30632f465d43b2e31b1c4
NaimanMpl <naimanmpl@gmail.com>
2024-10-28 19:57:19 +0100

    chore: removed lorem ipsum

--- src/playing/HelpDialog/index.tsx
+++ src/playing/HelpDialog/index.tsx
@@ -31,10 +31,30 @@ const HelpDialog = () => {
               {player?.role ?? 'Spectateur'}
             </span>
           </DialogDescription>
-          <span className="text-sm">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates
-            ipsa quia distinctio esse ab. Reprehenderit?
-          </span>
+          {player?.role && player.role === 'Evilman' && (
+            <span className="text-sm">
+              L’Evilman est le principal antagoniste, déterminé à bloquer le
+              joueur dans sa quête en plaçant des pièges sournois et des sorts
+              entravant sa progression. Son objectif ? Empêcher le joueur
+              d’atteindre le point d’arrivée à tout prix !
+            </span>
+          )}
+          {player?.role && player.role === 'Protector' && (
+            <span className="text-sm">
+              Le Protecteur est un allié essentiel pour le joueur, chargé de
+              contrecarrer les plans de l’Evilman. Son rôle est d’annuler les
+              pièges semés sur le chemin et d’appliquer des buffs stratégiques
+              au joueur.
+            </span>
+          )}
+          {!player?.role && (
+            <span className="text-sm">
+              Le Spectateur observe le déroulement de la partie sans interagir
+              directement avec le jeu. Ne pouvant poser d’actions, il profite
+              d’une vue d’ensemble des stratégies et des confrontations entre le
+              joueur, le Protecteur, et l’Evilman.
+            </span>
+          )}
         </DialogHeader>
         <DialogFooter>
           <DialogTrigger>
